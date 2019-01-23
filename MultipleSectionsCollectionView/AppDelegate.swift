c3cc1302bbcdbab4ea3d5bbb1d4a19f4a8c3fa98
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-23 13:00:47 -0800

    Organized the files.

--- MultipleSectionsCollectionView/AppDelegate.swift
+++ MultipleSectionsCollectionView/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
-        // USed to make a viewController progrmatically
+        // Used to make a viewController progrmatically
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
