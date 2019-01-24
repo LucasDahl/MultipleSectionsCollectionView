df97378d7bc898579bd371a5addb6a4164d0f67d
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-24 06:32:23 -0800

    Changed the TODO for future progress.

--- MultipleSectionsCollectionView/Cell/ImageCell.swift
+++ MultipleSectionsCollectionView/Cell/ImageCell.swift
@@ -87,7 +87,7 @@ class ImageCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionVie
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //TODO: - Add more viewControllers and images
+        //TODO: - Add more SKScene files for the top cells for the Albumcells.
     }
     
     private class IconsCell: UICollectionViewCell {
